2eddac0d96e17ef7ca3dbe26361c1a82d6c425a8
Sachidananda S V <sachivips@gmail.com>
2024-01-24 19:23:53 +0530

    mutler and cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from "cloudinary";
+import exp from "constants";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_CLOUD_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    //file upload successful message
+    console.log("file is uploaded on cloudinary", response.url);
+    return response;
+  } catch (err) {
+    fs.unlinkSync(localFilePath); //removes the locally saved file as the upload operation got failed
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
